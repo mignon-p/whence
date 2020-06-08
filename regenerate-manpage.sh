954b8b20ccc8fdcb761487a392c8817e8d7927fc
Patrick Pelletier <code@funwithsoftware.org>
2020-06-08 15:01:42 -0700

    Extract version from whence.h.

--- regenerate-manpage.sh
+++ regenerate-manpage.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+VERS=`perl -e 'while (<>) { print $1 if (/#define\s+CMD_VERSION\s+"(.*?)"/); }' whence.h`
+
 exec pod2man \
      --section=1 \
      --center="General Commands Manual" \
-     --release="whence 0.9" \
+     --release="whence $VERS" \
      --stderr \
      whence.pod whence.1
