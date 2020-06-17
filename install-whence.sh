496710f2695e8e8f843526b3dd14e791be018477
Patrick Pelletier <code@funwithsoftware.org>
2020-06-17 14:18:40 -0700

    Support install-whence.sh on Windows, too.

--- install-whence.sh
+++ install-whence.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-cp -p whence   /usr/local/bin             || exit $?
-cp -p whence.1 /usr/local/share/man/man1  || exit $?
+
+OS=`uname | sed -e 's/^[Mm][Ii][Nn][Gg][Ww].*/Windows/'`
+
+case $OS in
+    Windows) PREFIX="$HOME/AppData/Roaming/local";;
+    *)       PREFIX="/usr/local";;
+esac
+
+mkdir -p "$PREFIX/bin" "$PREFIX/share/man/man1" || exit $?
+cp -p whence   "$PREFIX/bin"                    || exit $?
+cp -p whence.1 "$PREFIX/share/man/man1"         || exit $?;;
