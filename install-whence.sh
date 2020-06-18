971d4dd7fc2304ffc8990d2941e42c727d9b1ecd
Patrick Pelletier <code@funwithsoftware.org>
2020-06-17 20:59:36 -0700

    Use .exe suffix on Windows.

--- install-whence.sh
+++ install-whence.sh
@@ -3,10 +3,12 @@
 OS=`uname | sed -e 's/^[Mm][Ii][Nn][Gg][Ww].*/Windows/'`
 
 case $OS in
-    Windows) PREFIX="$HOME/AppData/Roaming/local";;
-    *)       PREFIX="/usr/local";;
+    Windows) PREFIX="$HOME/AppData/Roaming/local"
+             EXE=".exe";;
+    *)       PREFIX="/usr/local"
+             EXE="";;
 esac
 
 mkdir -p "$PREFIX/bin" "$PREFIX/share/man/man1" || exit $?
-cp -p whence   "$PREFIX/bin"                    || exit $?
-cp -p whence.1 "$PREFIX/share/man/man1"         || exit $?
+cp -p whence$EXE "$PREFIX/bin"                  || exit $?
+cp -p whence.1   "$PREFIX/share/man/man1"       || exit $?
